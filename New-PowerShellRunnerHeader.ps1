1f1a2e78a5b118bbe21e2c923522bcc015f9a3bc
Lee Christensen <lee@localhost>
2017-04-11 09:19:49 -0700

    Initial commit

---
+++ New-PowerShellRunnerHeader.ps1
@@ -0,0 +1,41 @@
+﻿function New-PowerShellRunnerHeader
+{
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $AssemblyPath
+    )
+
+	$Bytes = Get-Content -Raw -Encoding Byte $Path
+	$OutputStr = New-Object System.Text.StringBuilder
+
+	$Counter = 1
+	foreach($Byte in $Bytes) {
+		$null = $OutputStr.Append("0x$('{0:X2}' -f $Byte),") 
+
+		if($Counter % 12 -eq 0) {
+			$null = $OutputStr.AppendLine()
+			$null = $OutputStr.Append("`t")
+		}
+		$Counter++
+	}
+
+	$null = $OutputStr.Remove($OutputStr.Length-1,1)
+
+	$Source = @"
+#ifndef POWERSHELLRUNNERDLL_H_
+#define POWERSHELLRUNNERDLL_H_
+
+static const unsigned char PowerShellRunner_dll[] = {
+    $($OutputStr.ToString())
+};
+
+static const unsigned int PowerShellRunner_dll_len = $($Bytes.Length);
+
+#endif
+"@
+}
